b96dae42da5acbe8f4ad3a42ef8efb872feb2c2a
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2026-02-24 21:25:09 +0100

    Create Model

---
+++ LapLog/Models/LapTime.cs
@@ -0,0 +1,15 @@
+﻿namespace LapLog.Models;
+
+public class LapTime
+{
+    public string TrackName { get; set; }
+    public string CarName { get; set; }
+    public TimeSpan Time { get; set; }
+
+    public LapTime(string trackName, string carName, TimeSpan time)
+    {
+        TrackName = trackName;
+        CarName = carName;
+        Time = time;
+    }
+}
