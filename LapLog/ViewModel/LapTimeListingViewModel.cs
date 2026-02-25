4c639a338d0e991b1f0324e230ec9af2a8260925
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2026-02-25 22:42:01 +0100

    Simplify viewmodel

--- LapLog/ViewModel/LapTimeListingViewModel.cs
+++ LapLog/ViewModel/LapTimeListingViewModel.cs
@@ -1,20 +1,19 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using LapLog.Models;
 
 namespace LapLog.ViewModel;
 
 public class LapTimeListingViewModel : ViewModelBase
 {
     private readonly ObservableCollection<LapTimeViewModel> _laptimes;
+    public IEnumerable<LapTimeViewModel> LapTimes => _laptimes;
 
     public LapTimeListingViewModel()
     {
         _laptimes = new ObservableCollection<LapTimeViewModel>();
-    }
-
-    public static LapTimeListingViewModel LoadViewModel()
-    {
-        LapTimeListingViewModel viewModel = new LapTimeListingViewModel();
-
-        return viewModel;
+        
+        _laptimes.Add(new LapTimeViewModel(new LapTime("Akina", "Toyota AE86", new TimeSpan(0, 4, 30))));
+        _laptimes.Add(new LapTimeViewModel(new LapTime("Akagi", "Mazda RX7", new TimeSpan(0, 4, 37))));
     }
 }
