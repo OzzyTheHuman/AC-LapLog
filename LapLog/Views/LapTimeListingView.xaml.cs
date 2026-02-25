127a71b5b821b53d96e756ea6fb5bdef14b139cb
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2026-02-25 23:07:53 +0100

    Create simple View

---
+++ LapLog/Views/LapTimeListingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LapLog.Views;
+
+public partial class LapTimeListingView : UserControl
+{
+    public LapTimeListingView()
+    {
+        InitializeComponent();
+    }
+}
