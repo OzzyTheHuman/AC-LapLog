7e48f6bff3c25a28c738eb152b7aa97db16b5dae
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2026-03-31 18:20:25 +0200

    feat: Custom button for navigation

---
+++ LapLog/Utilites/Btn.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LapLog.Utilites;
+
+public class Btn : RadioButton
+{
+    static Btn()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(Btn), new FrameworkPropertyMetadata(typeof(Btn)));
+    }
+}
